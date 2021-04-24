470fe146e97113cb3a5c2cd5e72a615c724d28df
LiangLliu <max1141847753@gmail.com>
2021-04-24 22:07:41 +0800

    初始化 service type

---
+++ src/main/resources/db/migration/V2021_03_07_20_05__init_data_service_type.sql
@@ -0,0 +1,7 @@
+INSERT INTO property_management.service_type (id, `type`, description, create_time, update_time) VALUES (1, 'complaint', '投诉', '2021-04-24 13:07:53', '2021-04-24 13:07:53');
+INSERT INTO property_management.service_type (id, `type`, description, create_time, update_time) VALUES (2, 'repair', '报修', '2021-04-24 13:07:53', '2021-04-24 13:07:53');
+INSERT INTO property_management.service_type (id, `type`, description, create_time, update_time) VALUES (3, 'clean', '清洁', '2021-04-24 13:07:53', '2021-04-24 13:07:53');
+INSERT INTO property_management.service_type (id, `type`, description, create_time, update_time) VALUES (4, 'householdElectrical', '家电维修', '2021-04-24 13:07:53', '2021-04-24 13:07:53');
+INSERT INTO property_management.service_type (id, `type`, description, create_time, update_time) VALUES (5, 'rushPipe', '管道疏通', '2021-04-24 13:07:53', '2021-04-24 13:07:53');
+INSERT INTO property_management.service_type (id, `type`, description, create_time, update_time) VALUES (6, 'cleanCloth', '干洗', '2021-04-24 13:07:53', '2021-04-24 13:07:53');
+INSERT INTO property_management.service_type (id, `type`, description, create_time, update_time) VALUES (7, 'unlocking', '开锁', '2021-04-24 13:07:53', '2021-04-24 13:07:53');
